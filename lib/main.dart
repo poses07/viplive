f7293ee2a495a9f1aa42b8b341394ad9993735c0
VipLiveDev <dev@viplive.com>
2026-03-11 12:11:55 +0300

    Request camera and mic permissions on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'screens/splash_screen.dart';
 import 'providers/user_provider.dart';
 // import 'services/zego_service.dart';
@@ -28,7 +29,33 @@ class VipLiveApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF66B4FF)),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const PermissionCheckWrapper(child: SplashScreen()),
     );
+  }
+}
+
+class PermissionCheckWrapper extends StatefulWidget {
+  final Widget child;
+  const PermissionCheckWrapper({super.key, required this.child});
+
+  @override
+  State<PermissionCheckWrapper> createState() => _PermissionCheckWrapperState();
+}
+
+class _PermissionCheckWrapperState extends State<PermissionCheckWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    _checkPermissions();
+  }
+
+  Future<void> _checkPermissions() async {
+    // Request Camera and Microphone permissions at app start
+    await [Permission.camera, Permission.microphone].request();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
